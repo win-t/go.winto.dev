8a75e436f65f3e811ae031e6b63d1e2f9d80ccc4
Kurnia D Win <kurnia.d.win@gmail.com>
2023-06-16 11:16:59 +0700

    update httpecho

--- httpecho/main.go
+++ httpecho/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"mime"
 	"net"
 	"net/http"
@@ -15,6 +16,8 @@ func main() {
 		port = "8080"
 	}
 
+	runExtraUDPEcho()
+
 	_, err := strconv.Atoi(port)
 	check(err)
 
@@ -23,6 +26,8 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got http request: %s > %s %s\n", r.RemoteAddr, r.Method, r.URL.EscapedPath())
+
 	cty, _, _ := mime.ParseMediaType(r.Header.Get("Content-Type"))
 	if strings.HasPrefix(cty, "text/") || r.ContentLength == 0 {
 		w.Header().Add("Content-Type", "text/plain")
@@ -40,6 +45,44 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	r.Write(w)
+}
+
+func runExtraUDPEcho() {
+	port := os.Getenv("UDPPORT")
+	if port == "" {
+		return
+	}
+
+	_, err := strconv.Atoi(port)
+	check(err)
+
+	addr, err := net.ResolveUDPAddr("udp", ":"+port)
+	check(err)
+
+	conn, err := net.ListenUDP("udp", addr)
+	check(err)
+
+	go func() {
+		var readBuf, writeBuf [64 << 10]byte // 64KiB
+		for {
+			n, peer, err := conn.ReadFromUDP(readBuf[:])
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "error read udp: %s\n", err.Error())
+				continue
+			}
+
+			fmt.Printf("got udp request: %s > size=%d\n", peer.String(), n)
+
+			dataIn := readBuf[:n]
+			dataOut := writeBuf[:0]
+
+			dataOut = append(dataOut, peer.String()...)
+			dataOut = append(dataOut, " > "...)
+			dataOut = append(dataOut, dataIn...)
+
+			_, _ = conn.WriteToUDP(dataOut, peer)
+		}
+	}()
 }
 
 func check(err error) {
