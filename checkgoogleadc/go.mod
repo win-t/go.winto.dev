ebb32d9a12549a39813122c1828f6006e4adc41a
Kurnia D Win <kurnia.d.win@gmail.com>
2023-06-20 07:07:15 +0700

    adding checkgoogleadc

---
+++ checkgoogleadc/go.mod
@@ -0,0 +1,18 @@
+module checkgoogleadc
+
+go 1.20
+
+require (
+	go.winto.dev/errors v1.1.1
+	go.winto.dev/mainpkg v1.0.2
+	golang.org/x/oauth2 v0.9.0
+)
+
+require (
+	cloud.google.com/go/compute/metadata v0.2.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	go.winto.dev/async v1.1.1 // indirect
+	golang.org/x/net v0.11.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
---
+++ checkgoogleadc/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+
+	"go.winto.dev/errors"
+	"go.winto.dev/mainpkg"
+	"golang.org/x/oauth2/google"
+)
+
+func main() { mainpkg.Exec(realmain) }
+
+func realmain() {
+	ctx := mainpkg.Context()
+
+	cred, err := google.FindDefaultCredentials(ctx, "email")
+	errors.Check(err)
+
+	token, err := cred.TokenSource.Token()
+	errors.Check(err)
+
+	req, err := http.NewRequestWithContext(ctx,
+		"GET",
+		fmt.Sprintf("https://oauth2.googleapis.com/tokeninfo?access_token=%s", url.QueryEscape(token.AccessToken)),
+		nil,
+	)
+	errors.Check(err)
+
+	resp, err := http.DefaultClient.Do(req)
+	errors.Check(err)
+
+	io.Copy(os.Stdout, resp.Body)
+}
