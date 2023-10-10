e51fd6b3ae3b86140e3d6b090d9afbcb0adfd66c
Kurnia D Win <kurnia.d.win@gmail.com>
2023-10-10 20:47:02 +0700

    update typecontext: avoid allocation and

--- typedcontext/typedcontext.go
+++ typedcontext/typedcontext.go
@@ -6,27 +6,29 @@ import (
 	"reflect"
 )
 
-type key struct{ t reflect.Type }
+type key[T any] struct{}
 
 // Create new context that have singleton value of the val's type.
 func New[T any](ctx context.Context, val T) context.Context {
-	t := reflect.TypeOf((*T)(nil)).Elem()
-	return context.WithValue(ctx, key{t}, val)
+	return context.WithValue(ctx, key[T]{}, val)
 }
 
 // Get the singleton value from the context.
 func Get[T any](ctx context.Context) (T, bool) {
-	t := reflect.TypeOf((*T)(nil)).Elem()
-	v, ok := ctx.Value(key{t}).(T)
+	v, ok := ctx.Value(key[T]{}).(T)
 	return v, ok
 }
 
 // like [Get] but panic if the value is not in the context.
 func MustGet[T any](ctx context.Context) T {
-	t := reflect.TypeOf((*T)(nil)).Elem()
-	v, ok := ctx.Value(key{t}).(T)
+	v, ok := ctx.Value(key[T]{}).(T)
 	if !ok {
-		panic("context doesn't have the singleton value of type " + t.String())
+		panicWrongType[T]()
 	}
 	return v
+}
+
+// panicWrongType is separate function to make MustGet got inlined in the caller
+func panicWrongType[T any]() {
+	panic("context doesn't have the singleton value of type " + reflect.TypeOf((*T)(nil)).Elem().String())
 }
