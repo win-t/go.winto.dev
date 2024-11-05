aa75ccd8d38a2d12b8ed8476190e133c61c62d7a
Kurnia D Win <kurnia.d.win@gmail.com>
2024-11-05 10:51:02 +0700

    update

--- httphandler/defmiddleware/basicauth.go
+++ httphandler/defmiddleware/basicauth.go
@@ -5,14 +5,18 @@ import (
 	"net/http"
 )
 
-func BasicAuth(verifyFn func(ctx context.Context, user, pass string) bool) func(http.HandlerFunc) http.HandlerFunc {
+func BasicAuth(verifyFn func(ctx context.Context, user, pass string) (ok bool, errmsg string)) func(http.HandlerFunc) http.HandlerFunc {
 	return func(next http.HandlerFunc) http.HandlerFunc {
 		return func(w http.ResponseWriter, r *http.Request) {
 			ctx := r.Context()
 			user, pass, _ := r.BasicAuth()
-			if !verifyFn(ctx, user, pass) {
+			ok, errmsg := verifyFn(ctx, user, pass)
+			if !ok {
+				if errmsg == "" {
+					errmsg = "Unauthorized"
+				}
 				w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
-				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				http.Error(w, errmsg, http.StatusUnauthorized)
 				return
 			}
 			next(w, r)
--- httphandler/defmiddleware/bearerheaderauth.go
+++ httphandler/defmiddleware/bearerheaderauth.go
@@ -6,14 +6,18 @@ import (
 	"strings"
 )
 
-func BearerHeaderAuth(verifyFn func(ctx context.Context, token string) bool) func(http.HandlerFunc) http.HandlerFunc {
+func BearerHeaderAuth(verifyFn func(ctx context.Context, token string) (ok bool, errmsg string)) func(http.HandlerFunc) http.HandlerFunc {
 	return func(next http.HandlerFunc) http.HandlerFunc {
 		return func(w http.ResponseWriter, r *http.Request) {
 			ctx := r.Context()
 			token := strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ")
-			if !verifyFn(ctx, token) {
+			ok, errmsg := verifyFn(ctx, token)
+			if !ok {
+				if errmsg == "" {
+					errmsg = "Unauthorized"
+				}
 				w.Header().Set("WWW-Authenticate", `Bearer realm="Restricted"`)
-				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				http.Error(w, errmsg, http.StatusUnauthorized)
 				return
 			}
 			next(w, r)
