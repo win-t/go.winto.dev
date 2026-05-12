c1affca321f265aacf20f76b016c66d39b593ecd
Kurnia D Win <kurnia.d.win@gmail.com>
2026-05-12 17:52:06 +0700

    update

--- jdig/marshal.go
+++ jdig/marshal.go
@@ -1,13 +1,12 @@
 package jdig
 
 import (
-	"bytes"
 	"encoding/json"
 	"strings"
 )
 
 // Wrap [json.Unmarshal], accept only string or []byte.
-func Unmarshal(data any, options ...func(*json.Decoder)) (any, error) {
+func Unmarshal(data any) (any, error) {
 	var input []byte
 	switch v := data.(type) {
 	case string:
@@ -18,27 +17,33 @@ func Unmarshal(data any, options ...func(*json.Decoder)) (any, error) {
 		panic("jdig: input must be string or []byte")
 	}
 	var v any
-	d := json.NewDecoder(bytes.NewReader(input))
-	for _, opt := range options {
-		opt(d)
-	}
-	err := d.Decode(&v)
+	err := json.Unmarshal(input, &v)
 	return v, err
 }
 
-func MustUnmarshal(data any, options ...func(*json.Decoder)) any {
-	v, err := Unmarshal(data, options...)
+func MustUnmarshal(data any) any {
+	v, err := Unmarshal(data)
 	if err != nil {
 		panic("jdig: unmarshal error: " + err.Error())
 	}
 	return v
 }
 
-func Marshal(v any, options ...func(*json.Encoder)) string {
+type MarshalOpt struct{ f func(*json.Encoder) }
+
+func SetEscapeHTML(on bool) MarshalOpt {
+	return MarshalOpt{f: func(e *json.Encoder) { e.SetEscapeHTML(on) }}
+}
+
+func SetIndent(prefix, indent string) MarshalOpt {
+	return MarshalOpt{f: func(e *json.Encoder) { e.SetIndent(prefix, indent) }}
+}
+
+func Marshal(v any, options ...MarshalOpt) string {
 	var sb strings.Builder
 	e := json.NewEncoder(&sb)
 	for _, opt := range options {
-		opt(e)
+		opt.f(e)
 	}
 
 	err := e.Encode(v)
